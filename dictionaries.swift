b7b5367002f490b2372f05b41bb294c65ca81f82
Beregszászi István <beistvan@gmail.com>
2023-07-28 10:40:04 +0200

    Dictionaries

---
+++ dictionaries.swift
@@ -0,0 +1,13 @@
+let heights = [
+    "Taylor Swift": 1.78,
+    "Ed Sheeran": 1.73
+]
+
+print(heights["Taylor Swift"])
+
+var roles = ["captain": "Mal", "engineer": "Kaylee"]
+let anotherHeights = ["Taylor Swift": 1.78]
+var address = ["road": "Park Lane", "city": "Cardiff"]
+var capitals = ["England": "London", "Scotland": "Edinburgh"]
+let forecast = ["Monday": "sunny", "Tuesday": "cloudy"]
+let scores = ["Sophie": 100]
