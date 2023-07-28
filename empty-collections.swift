ebd588e81e13038dc6b56c0dcfd778ff32e31f4e
Beregszászi István <beistvan@gmail.com>
2023-07-28 10:49:59 +0200

    Empty Collections

---
+++ empty-collections.swift
@@ -0,0 +1,20 @@
+let names = ["Eleanor", "Chidi", "Tahani", "Jianyu", "Michael", "Janet"]
+
+var teams = [String: String]()
+//add
+teams["Paul"] = "Red"
+
+var results = [Int]()
+
+var words = Set<String>()
+var numbers = Set<Int>()
+
+var scores = Dictionary<String, Int>()
+var results = Array<Int>()
+
+var favoriteColors = Dictionary<String, String>()
+var states = Set<String>()
+var answers = [Bool]()
+var highScores = Array<Int>()
+var temperatures = [Double]()
+var authorAges = [String: Int]()
