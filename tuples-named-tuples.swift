32b7ed021b07e226eaade455b13be2efd624c58e
Beregszászi István <beistvan@gmail.com>
2023-07-28 10:36:09 +0200

    Tuples and Named Tupless

---
+++ tuples-named-tuples.swift
@@ -0,0 +1,6 @@
+var website = (name: "Apple", url: "www.apple.com")
+var person = (name: "Paul", age: 40, isMarried: true)
+var name = (first: "Taylor", last: "Swift")
+
+print(name.0)
+print(name.frist)
