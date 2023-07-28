036192b122f05fa532e75481d06bd4f5b9cc0fde
Beregszászi István <beistvan@gmail.com>
2023-07-28 08:59:44 +0200

    Difference

---
+++ difference.swift
@@ -0,0 +1,8 @@
+let input = readLine()!.split(separator:" ")
+let first = input.first!
+let second = input.last!
+let firstInt = Int(first)!
+let secondInt = Int(second)!
+
+let result = firstInt-secondInt
+print(result)
