07c17ae66ee164852c4d161e8f837eed4cc198d8
Beregszászi István <beistvan@gmail.com>
2023-07-28 08:58:06 +0200

    Sum

---
+++ sum.swift
@@ -0,0 +1,8 @@
+let input = readLine()!.split(separator:" ")        
+let first = String(input.first!)                     
+let second = String(input.last!)                     
+let firstInt = Int(first)!                           
+let secondInt = Int(second)!                         
+
+let sum = firstInt + secondInt                         
+print(sum) 
