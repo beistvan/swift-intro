e0215d5b7d4048808074609d1be117d3bc18ddea
Beregszászi István <beistvan@gmail.com>
2023-07-28 10:19:51 +0200

    Type Annotations

---
+++ type-annotations.swift
@@ -0,0 +1,12 @@
+var percentage: Double = 99
+var name: String
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+var average: Double = 32.0
+var sales: Double = 100_000
+var populationMillions: Double = 12.5
+var distance: Double = 320
+var depth: Double = 10.2
+var score: Double = 1000
