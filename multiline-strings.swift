14383bf52c56df2879dc74815e81dbb4fdc73fa8
Beregszászi István <beistvan@gmail.com>
2023-07-28 10:02:14 +0200

    Multiline Strings

---
+++ multiline-strings.swift
@@ -0,0 +1,56 @@
+var quote = "Change the world by being yourself"
+var burns = """
+The best laid schemes
+O’ mice and men
+Gang aft agley
+"""
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+var burns = """
+The best laid schemes
+o' mice an' men
+gang aft agley
+"""
+var shakespeare = """
+To be or not to be
+that is the question
+"""
+var joseph = """
+When I am an old woman,
+I shall wear purple
+with a red hat that doesn't go,
+and doesn't suit me
+"""
+var tennyson = """
+Tis better to have loved
+and lost than never
+ to have loved at all
+"""
+var lear = """
+The Owl and the Pussy-cat went to sea
+In a beautiful pea-green boat,
+They took some honey, and plenty of money,
+Wrapped up in a five-pound note.
+"""
+var coleridge = """
+Water, water, everywhere
+and not a drop to drink
+"""
+var henley = """
+I am the master of my fate
+I am the captain of my soul
+"""
+var brooke = """
+If I should die,
+think only this of me
+There is some corner of a foreign field
+That is forever England.
+"""
