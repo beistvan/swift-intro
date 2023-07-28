91877e9c3564dbd0d6c58f25ca7e80660b2bf4c2
Beregszászi István <beistvan@gmail.com>
2023-07-28 10:24:21 +0200

    Arrays

---
+++ arrays.swift
@@ -0,0 +1,15 @@
+let john = "John Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+let beatles = [john, paul, george, ringo]
+
+print(beatles[1]) // Paul McCartney
+
+var temperatures = [32.0]
+var averages = [98.5, 97.1, 99.9]
+var cities: [String] = ["London", "Paris", "New York"]
+var readings: [Bool] = [false, false, true, false]
+var scores: [Int] = [10, 12, 9]
+var singers = ["Taylor", "Adele", "Justin"]
