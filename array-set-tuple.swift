ff71766378e2e043909757994f90533dd0454b41
Beregszászi István <beistvan@gmail.com>
2023-07-28 10:37:46 +0200

    Array, Set and Tuple

---
+++ array-set-tuple.swift
@@ -0,0 +1,5 @@
+let pythons = ["Eric", "Graham", "John", "Michael", "Terry", "Terry"]
+
+let set = Set(["aardvark", "astronaut", "azalea"])
+
+let address = (house: 555, street: "Taylor Swift Avenue", city: "Nashville")
