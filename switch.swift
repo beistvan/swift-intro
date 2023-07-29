f2c09bcedec3add8a0bd9134ef8b7dd9ce3fd251
Beregszászi István <beistvan@gmail.com>
2023-07-29 12:26:28 +0200

    Switch

---
+++ switch.swift
@@ -0,0 +1,30 @@
+var num = 20
+
+switch num {
+    case 10:
+        print("Given value is 10")
+    case 20:
+        print("Given value is 20")
+    case 30:
+        print("Given value is 30")
+    default:
+        print("No Matching")
+}
+
+let Timing = 19
+
+let time: String
+switch Timing {
+    case 0, 1, 2, 3, 4, 5:
+        time = "Early morning"
+    case 6, 7, 8, 9, 10, 11:
+        time = "Morning"
+    case 12, 13, 14, 15, 16:
+        time = "Afternoon"
+    case 17, 18, 19:
+        time = "Evening"
+    case 20, 21, 22, 23:
+        time = "Late evening"
+    default:
+        time = "INVALID Time!"
+}
